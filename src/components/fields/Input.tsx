12aa87f738613c9b468dace6c4ba04c6a7a15a19
ChukwukaHonour <ChukwukaHonour@gmail.com>
2024-08-30 19:23:24 +0100

    added the disabled property to the input component

--- src/components/fields/Input.tsx
+++ src/components/fields/Input.tsx
@@ -8,6 +8,7 @@ type InputFieldProps = {
   register: UseFormRegister<any>;
   required?: boolean;
   error?: string;
+  isDisabled?: boolean;
 };
 const Input = ({
   label,
@@ -16,6 +17,7 @@ const Input = ({
   name,
   register,
   required,
+  isDisabled,
 }: InputFieldProps) => {
   return (
     <div className="w-full flex flex-col gap-[5px]">
@@ -23,8 +25,13 @@ const Input = ({
       <input
         placeholder={placeholder}
         type="text"
+        readOnly={isDisabled}
         {...register(name, { required })}
-        className="placeholder:z-0 border-[2px] border-neutral-500 outline-none focus:border-blue-700 px-[10px] py-[5px] rounded-[3px]"
+        className={`${
+          isDisabled
+            ? "opacity-[0.8] text-neutral-950 bg-neutral-400 focus:none pointer-events-none"
+            : ""
+        } placeholder:z-0 border-[2px] border-neutral-500 outline-none focus:border-blue-700 px-[10px] py-[5px] rounded-[3px]`}
       />
       {error && <p className="text-red-500 text-xs mt-1">{error}</p>}
     </div>
