b2c806c4aad2f4ff98aa514c14bbf4ee39798155
ChukwukaHonour <ChukwukaHonour@gmail.com>
2024-08-30 19:58:37 +0100

    added onclick to the button component

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -5,11 +5,19 @@ type ButtonProps = {
   isLoading: boolean;
   isDisabled: boolean;
   type: "button" | "submit";
+  onClick?: () => void;
 };
 
-const Button = ({ children, isLoading, isDisabled, type }: ButtonProps) => {
+const Button = ({
+  children,
+  isLoading,
+  isDisabled,
+  type,
+  onClick,
+}: ButtonProps) => {
   return (
     <button
+      onClick={onClick}
       className={`disabled:opacity-[0.3] disabled:cursor-not-allowed bg-[#EF8730] py-[10px] px-[5px] rounded-[5px] text-neutral-50`}
       type={type}
       disabled={isDisabled}
