22098c9f4775675f5c94b57ca70c713c0a1b1ba6
ChukwukaHonour <ChukwukaHonour@gmail.com>
2024-08-30 15:31:25 +0100

    created the input component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,46 @@
+import Input from "./components/fields/Input";
+import Select from "./components/fields/Select";
+import Button from "./components/ui/Button";
+import { useForm, SubmitHandler } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { payoutFormSchema } from "./schema/PayoutFormSchema";
+import { z } from "zod";
+
+type PayoutFormInputs = z.infer<typeof payoutFormSchema>;
+
 function App() {
-  return <p className="">hello world</p>;
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<PayoutFormInputs>({
+    resolver: zodResolver(payoutFormSchema),
+  });
+
+  const onSubmit = () => {};
+  return (
+    <main className="bg-[#D2D8DE] flex justify-center items-center h-[100vh]">
+      <div className="bg-neutral-50 w-[600px] h-[600px] rounded-[8px] py-[20px] px-[20px]">
+        <h2 className="text-center text-[30px] font-[600]">Payout Form</h2>
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="flex flex-col gap-[10px] mt-[20px]"
+        >
+          <Input
+            name="destinationAccountNumber"
+            type="text"
+            placeholder="Account Number"
+            register={register}
+            required
+            error={errors.destinationAccountNumber?.message}
+            label="Destination Account Number"
+          />
+          <Select />
+          <Button />
+        </form>
+      </div>
+    </main>
+  );
 }
 
 export default App;
--- src/components/fields/Input.tsx
+++ src/components/fields/Input.tsx
@@ -1,5 +1,34 @@
-const Input = () => {
-  return <div>Input Field</div>;
+import { UseFormRegister } from "react-hook-form";
+
+type InputFieldProps = {
+  label: string;
+  name: string;
+  type: string;
+  placeholder: string;
+  register: UseFormRegister<any>;
+  required?: boolean;
+  error?: string;
+};
+const Input = ({
+  label,
+  error,
+  placeholder,
+  name,
+  register,
+  required,
+}: InputFieldProps) => {
+  return (
+    <div className="flex flex-col gap-[5px] max-w-[400px]">
+      <label>{label}</label>
+      <input
+        placeholder={placeholder}
+        type="text"
+        {...register(name, { required })}
+        className="border-[2px] border-neutral-500 outline-none focus:border-blue-700 px-[10px] py-[5px] rounded-[3px]"
+      />
+      {error && <p className="text-red-500 text-xs mt-1">{error}</p>}
+    </div>
+  );
 };
 
 export default Input;
