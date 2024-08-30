99ff296ffd6ec816e55292664c112f2ef380f503
ChukwukaHonour <ChukwukaHonour@gmail.com>
2024-08-30 20:24:19 +0100

    minor edit

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,13 @@ import App from "./App.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "./index.css";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
