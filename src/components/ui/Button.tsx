606637ebe4f63f702c90e1d2cc0802a14c565bbe
ChukwukaHonour <ChukwukaHonour@gmail.com>
2024-08-30 18:49:38 +0100

    created the button component

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,5 +1,22 @@
-const Button = () => {
-  return <div>Button component</div>;
+import React from "react";
+
+type ButtonProps = {
+  children: React.ReactNode;
+  isLoading: boolean;
+  isDisabled: boolean;
+  type: "button" | "submit";
+};
+
+const Button = ({ children, isLoading, isDisabled, type }: ButtonProps) => {
+  return (
+    <button
+      className={`bg-[#EF8730] py-[10px] px-[5px] rounded-[5px] text-neutral-50`}
+      type={type}
+      disabled={isDisabled}
+    >
+      {isLoading ? <>Loading...</> : children}
+    </button>
+  );
 };
 
 export default Button;
