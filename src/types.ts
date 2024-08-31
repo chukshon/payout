0346e82398aa3d9d3b54d6dd342d3a1d2e2ea3d0
ChukwukaHonour <ChukwukaHonour@gmail.com>
2024-08-31 16:02:51 +0100

    added the Bank type

--- src/types.ts
+++ src/types.ts
@@ -2,3 +2,13 @@ export type optionT = {
   label: string;
   value: string;
 };
+
+export type BankT = {
+  bankId: string | null;
+  baseUssdCode: string | null;
+  code: string;
+  name: string;
+  nipBankCode: string;
+  transferUssdTemplate: string | null;
+  ussdTemplate: string | null;
+};
