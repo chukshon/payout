983d1d4d0446e951ba4315f2c50c977cff556441
ChukwukaHonour <ChukwukaHonour@gmail.com>
2024-09-01 09:54:58 +0100

    minor edit to the schema

--- src/schema/PayoutFormSchema.ts
+++ src/schema/PayoutFormSchema.ts
@@ -3,14 +3,11 @@ import { z } from "zod";
 export const payoutFormSchema = z.object({
   destinationAccountNumber: z
     .string()
-    .min(1, { message: "Account Number is too short" })
+    .min(10, { message: "Account Number must be a 10 digit number" })
     .max(10, { message: "Account Number is too long" }),
   bankCode: z.string().min(1, { message: "Please select a bank" }),
   bankName: z.string(),
   amountToBePaid: z
     .string()
     .min(1, { message: "Amount to be paid  is required" }),
-  destinationAccountName: z
-    .string()
-    .min(1, { message: "Account Name is required" }),
 });
