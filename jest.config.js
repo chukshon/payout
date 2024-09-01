f5515231c8b807b84a3f1a6091c3b69e9c1fa6ee
ChukwukaHonour <ChukwukaHonour@gmail.com>
2024-09-01 12:12:02 +0100

    added the jest config file

---
+++ jest.config.js
@@ -0,0 +1,20 @@
+const config = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  roots: ["<rootDir>/src", "<rootDir>/src/tests"],
+  testMatch: ["**/__tests__/**/*.ts?(x)", "**/?(*.)+(spec|test).ts?(x)"],
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  transform: {
+    "^.+\\.tsx?$": "ts-jest",
+  },
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1",
+  },
+  globals: {
+    "ts-jest": {
+      tsconfig: "tsconfig.json",
+    },
+  },
+};
+
+export default config;
